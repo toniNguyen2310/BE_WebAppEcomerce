7c5bf8af261be732ae93f58186be034c03c40840
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-24 13:43:01 +0700

    update 6th

--- index.js
+++ index.js
@@ -39,27 +39,35 @@ app.use(
 // });
 
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://lacdau-clone-fe-pj.vercel.app/"
+//   );
+
+//   // Request methods you wish to allow
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.header("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
+app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://lacdau-clone-fe-pj.vercel.app/"
+    "https://lacdau-clone-fe-pj.vercel.app"
   );
-
-  // Request methods you wish to allow
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
   next();
 });
 

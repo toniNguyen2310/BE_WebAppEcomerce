4df03998c4d328537026951daf9906160900dd40
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-24 16:51:50 +0700

    update 16th

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://lacdau-clone-fe-pj.vercel.app"
+    "https://lacdau-clone-fe-pj.vercel.app, https://lacdau-clone-fe-pj.vercel.app/admin"
   );
 
   // Request methods you wish to allow

5b5aef32cdb7d484a466813f79710c19ce75c7fe
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-24 14:11:07 +0700

    9th

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ app.use(
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();

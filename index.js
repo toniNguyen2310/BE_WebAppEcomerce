9f0c1dbf478242571f4b695ff38d8de4ca247ea9
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-24 13:53:35 +0700

    update 6th

--- index.js
+++ index.js
@@ -68,6 +68,8 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://lacdau-clone-fe-pj.vercel.app"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 

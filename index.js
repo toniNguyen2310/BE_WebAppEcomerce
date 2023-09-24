aa297de147804c4b3efb2d027d41da7948bb5533
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-24 13:26:06 +0700

    update config

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(
 // app.use(cors());
 
 app.all("/*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "/");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });

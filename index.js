eb13db767d7fd214483bcfb273d7afece2b233d1
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-24 13:23:41 +0700

    cors all

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ app.use(
 
 // app.use(cors());
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://lacdau-clone-fe-pj.vercel.app"
-  );
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 

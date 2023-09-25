493f7665e570cd73fa658c618843bc46b823644f
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-25 15:03:05 +0700

    credential true

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
 
+  res.setHeader("Access-Control-Allow-Credentialstrue", true);
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",

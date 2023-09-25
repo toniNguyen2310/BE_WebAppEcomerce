0d27604fbb764a286cba81cabe4771a00ca1edd7
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-25 14:52:24 +0700

    console log method header

--- index.js
+++ index.js
@@ -25,16 +25,6 @@ app.use(
   })
 );
 
-// app.all("/*", function (req, res, next) {
-//   console.log("req>>> ", req);
-//   res.header("Access-Control-Allow-Origin", "http://localhost:5174");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,Authorization,content-type"
-//   );
-//   next();
-// });
-
 // app.use(cors());
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -42,32 +32,32 @@ app.use(
 // });
 
 // Add headers before the routes are defined
-// app.use(function (req, res, next) {
-//   console.log("req.method>> ", req.method, req.headers.origin);
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "*");
+app.use(function (req, res, next) {
+  console.log("req.method>> ", req.method, req.headers);
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type, Authorization"
-//   );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Authorization"
+  );
 
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-//   // Pass to next layer of middleware
-//   next();
-// });
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  // Pass to next layer of middleware
+  next();
+});
 
 // app.use((req, res, next) => {
 //   res.header(

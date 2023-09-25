205da115eed88f9da5adf26b4d93e58792846fa1
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-25 15:23:06 +0700

    update *

--- index.js
+++ index.js
@@ -26,15 +26,15 @@ const hostname = process.env.HOST_NAME;
 // );
 
 app.use(cors());
-app.all("*", function (req, res, next) {
+app.all("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "origin, content-type, accept");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-
-  if (req.method == "OPTIONS") {
-    res.status(200).end();
-  }
+  // res.header("Access-Control-Allow-Headers", "origin, content-type, accept");
+  // res.header("Access-Control-Allow-Credentials", "true");
+  // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  // if (req.method == "OPTIONS") {
+  //   res.status(200).end();
+  // }
 
   next();
 });

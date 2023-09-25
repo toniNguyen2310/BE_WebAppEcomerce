113ee8a16109fe0a95a20d67a0c1a25ec22baa6a
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-09-25 15:07:32 +0700

    delete credential

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  res.setHeader("Access-Control-Allow-Credentialstrue", true);
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -52,7 +51,8 @@ app.use(function (req, res, next) {
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  // res.setHeader("Access-Control-Allow-Credentials", true);
+
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
